aed710a8321c4753bde99373c98a6936e274ec11
nyrrd <29787812-nyrrd@users.noreply.gitlab.com>
2026-03-28 12:11:35 +0200

    uncommented dirs with no data

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,10 @@ export default defineConfig({
 				{
 					label: 'Commands',
 					autogenerate: { directory: 'commands' },
+					//label: 'Cheatsheets',
+					//autogenerate: { directory: 'cheatsheets' },
+					//label: 'Snippets',
+					//autogenerate: { directory: 'snippets' },
 				},
 			],
 		}),
