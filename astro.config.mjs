5cec93116caac03441169066a2cb8f17f4befeb6
nyrrd <29787812-nyrrd@users.noreply.gitlab.com>
2026-04-30 12:00:54 +0200

    Adding new content directory - development

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,32 +5,31 @@ import gruvbox from 'starlight-theme-gruvbox';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'dashdashhelp',
-			plugins: [gruvbox()],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nyrrd/dashdashhelp' }],
-			sidebar: [
-				{
-					label: 'Environment Variables',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'XDG', slug: 'environment-variables/xdg' },
-						{ label: 'Shell', slug: 'environment-variables/shell' },
-						{ label: 'Tools', slug: 'environment-variables/tools' },
-						{ label: 'Proprietary Tools', slug: 'environment-variables/proprietary' },
-
-					],					
-				},
-				{
-					label: 'Commands',
-					autogenerate: { directory: 'commands' },
-					//label: 'Cheatsheets',
-					//autogenerate: { directory: 'cheatsheets' },
-					//label: 'Snippets',
-					//autogenerate: { directory: 'snippets' },
-				},
-			],
-		}),
+integrations: [
+starlight({
+title: 'dashdashhelp',
+plugins: [gruvbox()],
+social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nyrrd/dashdashhelp' }],
+sidebar: [{
+	label: 'Environment Variables',
+	items: [
+		// Each item here is one entry in the navigation menu.
+		{ label: 'XDG', slug: 'environment-variables/xdg' },
+		{ label: 'Shell', slug: 'environment-variables/shell' },
+		{ label: 'Development', slug: 'environment-variables/development'},
+		{ label: 'Tools', slug: 'environment-variables/tools' },
+		{ label: 'Proprietary Tools', slug: 'environment-variables/proprietary' },
+		],					
+	},
+	{
+	label: 'Commands',
+	autogenerate: { directory: 'commands' },
+		//label: 'Cheatsheets',
+		//autogenerate: { directory: 'cheatsheets' },
+		//label: 'Snippets',
+		//autogenerate: { directory: 'snippets' },
+	},
 	],
+}),
+],
 });
---
+++ src/content/docs/environment-variables/development.md
@@ -0,0 +1,11 @@
+---
+title: Development
+description: Environment variables for various development tools and packages.
+---
+
+**Node:**
+```bash
+
+```
+
+
